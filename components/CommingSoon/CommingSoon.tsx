5ebb0d08d9a2ec76cccfc6f90c5882a57ab7d266
SahilSharma2911 <sahilsharmarksh@gmail.com>
2025-05-26 22:44:52 +0530

    comming soon fix

--- components/CommingSoon/CommingSoon.tsx
+++ components/CommingSoon/CommingSoon.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ComingSoon = () => {
     return (
-        <div className='min-h-[88vh] md:min-h-[82vh] flex flex-col gap-5 items-center justify-center'>
-            <h1 className='-mt-14 font-unbounded text-4xl md:text-5xl font-bold bg-gradient-to-r from-[#3E2FE1] to-[#8C14AC] bg-clip-text text-transparent text-center'>
+        <div className='min-h-[88vh] md:min-h-[82vh] flex flex-col gap-3 items-center justify-center'>
+            <h1 className='-mt-14 md:py-2 font-unbounded text-4xl md:text-5xl font-bold bg-gradient-to-r from-[#3E2FE1] to-[#8C14AC] bg-clip-text text-transparent text-center'>
                 Coming Soon
             </h1>
             <p className='text-lg text-gray-600 text-center  px-4'>
