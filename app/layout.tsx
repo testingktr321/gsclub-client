d692ff22d4695b7ebeea8117087610fa055241d5
SahilSharma2911 <sahilsharmarksh@gmail.com>
2025-07-11 11:40:14 +0530

    done added analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TLGTR33M');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Yandex.Metrika counter */}
         <script
           type="text/javascript"
@@ -100,8 +114,8 @@ export default function RootLayout({
         <noscript>
           <div>
             <Image
-            width={1000}
-            height={1000}
+              width={1000}
+              height={1000}
               src="https://mc.yandex.ru/watch/102308489"
               style={{ position: 'absolute', left: '-9999px' }}
               alt=""
@@ -113,6 +127,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased ${unbounded.variable}`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TLGTR33M"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Providers>
           <Navbar />
           <Toaster position="top-right" reverseOrder={false} />
