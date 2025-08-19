3c25933de03ecf703bb542a2354f90c05e0cacca
SahilSharma2911 <sahilsharmarksh@gmail.com>
2025-08-19 14:55:57 +0530

    figma chage upto date ho gya

--- components/adult-goods/AdultPage.tsx
+++ components/adult-goods/AdultPage.tsx
@@ -1,14 +1,14 @@
 "use client"
 import React from 'react'
 import Hero from './Hero'
-// import Products from '../HomePage/Products'
+import Products from '../HomePage/Products'
 
 const AdultPage = () => {
     return (
         <main>
             <Hero />
             <div className='pt-16'>
-                {/* <Products productType="ADULT_GOODS" /> */}
+                <Products productType="ADULT_GOODS" />
             </div>
         </main>
     )
